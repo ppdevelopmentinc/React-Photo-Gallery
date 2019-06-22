6dd29713137994ef50cbcf4891260c4518b2319e
Priscilla Pang <Priscilla.pang@keystonelaw.co.uk>
2019-06-22 19:22:51 +0100

    Updates

---
+++ precache-manifest.ff5feeeb782a5b73240f29a4864efe06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee7f6bdd966d80b392f081097d6c40b",
+    "url": "/React-Photo-Gallery/index.html"
+  },
+  {
+    "revision": "fa15db95f586be3cec8a",
+    "url": "/React-Photo-Gallery/static/css/main.ce48e348.chunk.css"
+  },
+  {
+    "revision": "0d0b59929e7535e2f0e9",
+    "url": "/React-Photo-Gallery/static/js/2.4ddc5771.chunk.js"
+  },
+  {
+    "revision": "fa15db95f586be3cec8a",
+    "url": "/React-Photo-Gallery/static/js/main.7167a765.chunk.js"
+  },
+  {
+    "revision": "37b3dedae0477537fc44",
+    "url": "/React-Photo-Gallery/static/js/runtime~main.9c1cc067.js"
+  }
+]);
--- src/components/Config.js
+++
@@ -1,2 +0,0 @@
-const apiKey = "c4c5f0be3de7790953f90adb667c8254";
-export default apiKey;
